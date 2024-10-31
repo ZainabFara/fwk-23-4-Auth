84335edcb31e0522dd4c26407fad2a32b9702533
Daniella Alolo <daniella.alolo@hotmail.com>
2024-10-31 22:22:20 +0100

    Added helmet

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const app = express();
 app.use(express.json());
 const cors = require("cors");
 const { AUTH, AUTH_TYPES } = require("./config");
+const helmet = require("helmet");
+
+app.use(helmet.hidePoweredBy());
 
 app.use(
-    cors({
-        origin: "http://localhost:6006",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:6006",
+    credentials: true,
+  })
 );
 
 app.use(express.json());
